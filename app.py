86f77b2fc7710fe1c648c5fd1ff5f6e544500046
Huzaifa Rashid <huzaifaras10@gmail.com>
2025-01-27 06:43:25 -0800

    update streamlit

--- app.py
+++ app.py
@@ -6,5 +6,7 @@ from tensorflow.keras.models import load_model
 
 
 st.title('Predictive Student Dropout Prevention System')
-st.write('this is the web app for the predictive student dropout prevention system. Upload your Excel or CSV file to get started.')
-
+st.write("""This web application is designed to help educational institutions predict and prevent student dropouts. 
+By leveraging machine learning models, the system analyzes student data to identify those at risk of dropping out. 
+Upload your Excel or CSV file containing student data to get started. The application will process the data, 
+make predictions, and display the results to help you take proactive measures.""")
